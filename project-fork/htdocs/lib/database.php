0dd52d706ea9b33dcc35d1c70ad0ffdcd35946ec
TheSkibb <kfskibrek@icloud.com>
2022-12-14 13:16:36 +0100

    rename and updates

--- project-fork/htdocs/lib/database.php
+++ project-fork/htdocs/lib/database.php
@@ -1,10 +1,4 @@
 <?php 
-#$host = "localhost";  
-#$bruker = "root";  
-#$passord = "";  
-#$db_navn = "prosjekt"; 
-
-#$con = mysqli_connect($host, $bruker, $passord, $db_navn);
 $mysql_hostname = "172.17.0.2";
 $mysql_user = "php";
 $mysql_password = "123";
@@ -12,6 +6,4 @@ $mysql_database = "project";
 $errors = array();
 
 $con = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password, $mysql_database);
-
-#$con = mysqli_connect("localhost","root","","prosjekt");
 ?>
